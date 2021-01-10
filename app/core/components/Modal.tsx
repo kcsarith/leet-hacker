ec04a3b757af06c751a6bf5437added01fccff32
kcsarith <krisnasarith@gmail.com>
2021-01-10 15:57:54 +0900

    Update Modal.tsx

--- app/core/components/Modal.tsx
+++ app/core/components/Modal.tsx
@@ -24,6 +24,7 @@ export const Modal = ({
             <div
               className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
               aria-hidden="true"
+              onClick={()=>setShowModal(false)}
             ></div>
             <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
               &#8203;
