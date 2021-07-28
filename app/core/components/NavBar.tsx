51f3174cb39f9312df592b7a985983b41f7e2001
kcsarith <krisnasarith@gmail.com>
2021-07-28 07:46:07 -0700

    Hide Leet Hacker title on nav on small screens

--- app/core/components/NavBar.tsx
+++ app/core/components/NavBar.tsx
@@ -52,75 +52,99 @@ const NavBarRight = () => {
   if (currentUser) {
     return (
       <>
-        <div className="text-white hidden md:inline">User name: {currentUser.email}</div>
-        <NavButton link={Routes.HackPadsPage()} buttonText="Hack!" />
-        <NavButton
-          onClickEvent={async () => {
-            await logoutMutation()
-          }}
-          link="/"
-          buttonText="Logout"
-        />
+        {" "}
+        <div className="flex items-center justify-around h-8">
+          <div className="flex items-center justify-around w-full min-h-full">
+            <div className="flex-shrink-0 text-white text-xl">
+              <Link href="/">Leet Hacker</Link>
+            </div>
+            <div className="md:block">
+              <div className="ml-10 flex items-baseline space-x-4">
+                <div className="text-white hidden md:inline">User name: {currentUser.email}</div>
+                <NavButton link={Routes.HackPadsPage()} buttonText="Hack!" />
+                <NavButton
+                  onClickEvent={async () => {
+                    await logoutMutation()
+                  }}
+                  link="/"
+                  buttonText="Logout"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </>
     )
   } else {
     return (
       <>
-        <button
-          className="bg-green-500 text-white active:bg-red-600 font-bold uppercase text-sm px-3 m-h-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-          type="button"
-          onClick={async () => {
-            await loginMutation({ email: "demolition@demo.com", password: "demolitionman" })
-          }}
-        >
-          DEMOLOGIN
-        </button>
-        <NavButton
-          onClickEvent={async () => {
-            await setShowLoginModal(true)
-          }}
-          buttonText="Login"
-        />
-        <Modal
-          modalTitle="Login"
-          modalBody={
-            <LoginForm
-              onSuccess={() => {
-                router.push(Routes.HackPadsPage())
-                setShowLoginModal(false)
-              }}
-            />
-          }
-          // submitButtonName="Submit"
-          // closeButtonName="Close"
-          showModal={showLoginModal}
-          setShowModal={setShowLoginModal}
-          submitButtonAction={undefined}
-          closeButtonAction={undefined}
-        />
-        <NavButton
-          onClickEvent={async () => {
-            await setShowSignupModal(true)
-          }}
-          buttonText="Register"
-        />
-        <Modal
-          modalTitle="Register"
-          modalBody={
-            <SignupForm
-              onSuccess={() => {
-                router.push(Routes.HackPadsPage())
-                setShowSignupModal(false)
-              }}
-            />
-          }
-          // submitButtonName="Submit"
-          // closeButtonName="Close"
-          showModal={showSignupModal}
-          setShowModal={setShowSignupModal}
-          submitButtonAction={undefined}
-          closeButtonAction={undefined}
-        />
+        {" "}
+        <div className="flex items-center justify-around h-8">
+          <div className="flex items-center justify-around w-full min-h-full">
+            <div className="flex-shrink-0 text-white text-xl hidden sm:inline">
+              <Link href="/">Leet Hacker</Link>
+            </div>
+            <div className="md:block">
+              <div className="ml-10 flex items-baseline space-x-4">
+                <button
+                  className="bg-green-500 text-white active:bg-red-600 font-bold uppercase text-sm px-3 m-h-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                  type="button"
+                  onClick={async () => {
+                    await loginMutation({ email: "demolition@demo.com", password: "demolitionman" })
+                  }}
+                >
+                  DEMOLOGIN
+                </button>
+                <NavButton
+                  onClickEvent={async () => {
+                    await setShowLoginModal(true)
+                  }}
+                  buttonText="Login"
+                />
+                <Modal
+                  modalTitle="Login"
+                  modalBody={
+                    <LoginForm
+                      onSuccess={() => {
+                        router.push(Routes.HackPadsPage())
+                        setShowLoginModal(false)
+                      }}
+                    />
+                  }
+                  // submitButtonName="Submit"
+                  // closeButtonName="Close"
+                  showModal={showLoginModal}
+                  setShowModal={setShowLoginModal}
+                  submitButtonAction={undefined}
+                  closeButtonAction={undefined}
+                />
+                <NavButton
+                  onClickEvent={async () => {
+                    await setShowSignupModal(true)
+                  }}
+                  buttonText="Register"
+                />
+                <Modal
+                  modalTitle="Register"
+                  modalBody={
+                    <SignupForm
+                      onSuccess={() => {
+                        router.push(Routes.HackPadsPage())
+                        setShowSignupModal(false)
+                      }}
+                    />
+                  }
+                  // submitButtonName="Submit"
+                  // closeButtonName="Close"
+                  showModal={showSignupModal}
+                  setShowModal={setShowSignupModal}
+                  submitButtonAction={undefined}
+                  closeButtonAction={undefined}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </>
     )
   }
@@ -130,20 +154,9 @@ export const NavBar = () => {
   return (
     <nav className="bg-gradient-to-l from-blue-800 via-green-800 to-purple-500">
       <div className="mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-around h-8">
-          <div className="flex items-center justify-around w-full min-h-full">
-            <div className="flex-shrink-0 text-white text-xl">
-              <Link href="/">Leet Hacker</Link>
-            </div>
-            <div className="md:block">
-              <div className="ml-10 flex items-baseline space-x-4">
-                <Suspense fallback="Loading...">
-                  <NavBarRight />
-                </Suspense>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Suspense fallback="Loading...">
+          <NavBarRight />
+        </Suspense>
       </div>
     </nav>
   )
